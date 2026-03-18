547da73c01e25d68c1e7c342b015b6f86bca0c38
EJ Gordon <ejgordoncode@gmail.com>
2026-03-17 17:12:51 -0700

    [Q2] [Time Beats: 55.07%] [Memory Beats: 83.35%] - LeetPush

--- Q2.-Shuffle-the-Array.py
+++ Q2.-Shuffle-the-Array.py
@@ -1,11 +1,8 @@
 1class Solution:
 2    def shuffle(self, nums: List[int], n: int) -> List[int]:
-3        ans = [0] * 2 * n
+3        ans = []
 4        x_index = 0
-5        y_index = n
-6        while x_index < n:
-7            ans[x_index*2:x_index*2 + 2] = ([nums[x_index], nums[y_index]])
-8            x_index += 1
-9            y_index += 1
-10        return ans
-11
+5        for x_index in range(n):
+6            ans.extend([nums[x_index], nums[x_index + n]])
+7        return ans
+8
