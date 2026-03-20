67458ae1a01031c68ffefe6efdb17095512c948c
EJ Gordon <ejgordoncode@gmail.com>
2026-03-19 18:07:46 -0700

    [Q1] [Time Beats: 100.00%] [Memory Beats: 97.46%] - LeetPush

--- Q1.-Build-an-Array-With-Stack-Operations.py
+++ Q1.-Build-an-Array-With-Stack-Operations.py
@@ -1,25 +1,24 @@
 1class Solution:
 2    def buildArray(self, target: List[int], n: int) -> List[str]:
-3        s = deque() # stack s
-4        operations = [] # pushes and pops taken to get to target
-5        i = 0
-6        current_stream_element = 0
-7        for num in range(1,n+1):
-8            operations.append("Push")
-9            
-10            if num != target[i]:
-11                operations.append("Pop")
-12            else:
-13                i += 1
-14
-15            if num == target[-1]:
-16                return operations
-17            
+3        operations = [] # pushes and pops taken to get to target
+4        i = 0
+5        current_stream_element = 0
+6        for num in range(1,n+1): # range represents stream of ints from 1 to n inclusive
+7            operations.append("Push")
+8            
+9            if num != target[i]:
+10                operations.append("Pop")
+11            else:
+12                i += 1
+13
+14            if num == target[-1]:
+15                return operations
+16            
+17
 18
-19
+19            
 20            
-21            
-22
-23            
+21
+22            
+23        
 24        
-25        
